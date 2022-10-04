ff628f1c0f7a7413d08d65f2184e73f541bb9b0f
PedroTeochi <teochi2003@gmail.com>
2022-10-04 10:44:36 -0300

    Aula 04/10

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
 
 @Component({
   selector: 'app-heroes',
@@ -8,13 +9,21 @@ import { Hero } from '../hero';
 })
 export class HeroesComponent implements OnInit {
 
+    heroes = HEROES;
+
      hero: Hero = {
      id: 1,
      name: 'WindStorm'
+   
     };
   constructor() { }
 
   ngOnInit(): void {
   }
+  selectedHero?: Hero;
+onSelect(hero: Hero): void {
+  this.selectedHero = hero;
+}
+
 
 }
